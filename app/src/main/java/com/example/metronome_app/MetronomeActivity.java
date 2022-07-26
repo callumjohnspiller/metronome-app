474e18ba2e7ba71e1add232bfa3a4e6b444ca68e
Callum Spiller <callum.spiller@sony.com>
2022-07-26 14:03:40 +0100

    added strings. Renamed main activity. Added button

--- app/src/main/java/com/example/metronome_app/MetronomeActivity.java
+++ app/src/main/java/com/example/metronome_app/MetronomeActivity.java
@@ -3,7 +3,7 @@ package com.example.metronome_app;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MetronomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
