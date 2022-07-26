05760d00b286fd31db01e7174bf8a0fb53dab115
Callum Spiller <callum.spiller@sony.com>
2022-07-26 14:20:49 +0100

    button switches between start and stop

--- app/src/main/java/com/example/metronome_app/MetronomeActivity.java
+++ app/src/main/java/com/example/metronome_app/MetronomeActivity.java
@@ -1,13 +1,36 @@
 package com.example.metronome_app;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MetronomeActivity extends AppCompatActivity {
+
+    private Boolean metronomeRunning = false;
+    private Button startStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startStop = findViewById(R.id.metronomeStartStop);
+
+        startStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                metronomeToggle();
+            }
+        });
+    }
+
+    private void metronomeToggle() {
+        metronomeRunning = !metronomeRunning;
+        if (metronomeRunning) {
+            startStop.setText(R.string.metronome_stop);
+        } else {
+            startStop.setText(R.string.metronome_start);
+        }
     }
 }
