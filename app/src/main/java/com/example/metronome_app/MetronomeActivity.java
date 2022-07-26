0856b4db89a9a7ee9e3295427461361d452f4aa9
Callum Spiller <callum.spiller@sony.com>
2022-07-26 17:40:49 +0100

    basic two page app with text sending

--- app/src/main/java/com/example/metronome_app/MetronomeActivity.java
+++ app/src/main/java/com/example/metronome_app/MetronomeActivity.java
@@ -1,28 +1,34 @@
 package com.example.metronome_app;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.EditText;
 
 public class MetronomeActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.metronome_app.MESSAGE";
     private Boolean metronomeRunning = false;
     private Button startStop;
-
+    private Button send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        send = findViewById(R.id.sendButton);
+        send.setOnClickListener(v -> sendMessage(send));
         startStop = findViewById(R.id.metronomeStartStop);
+        startStop.setOnClickListener(v -> metronomeToggle());
 
-        startStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                metronomeToggle();
-            }
-        });
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     private void metronomeToggle() {
